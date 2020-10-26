efe7d3faeeb0ef082b39e3d7b76eeba61da5321f
sina973 <44430322+sina973@users.noreply.github.com>
2020-10-26 16:49:09 +0330

    Nodes fixed

--- Scanner.py
+++ Scanner.py
@@ -26,7 +26,69 @@ Nodes = []
 state = 0
 lexeme = ''
 symbol_list = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return']
+file = open("input.txt", 'r')
+input_file = file.read()
+file.close()
+while_state = True
+pointer = 0
+line = 0
+tokens = []
 
+
+
+for i in range(0, 16):
+    Nodes.append(Node(i))
+
+# #############  Add exit edges of each node ##################
+
+Nodes[0].add_to_edges(1, ["letter"])
+Nodes[0].add_to_edges(3, ["digit"])
+Nodes[0].add_to_edges(5, [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<'])
+Nodes[0].add_to_edges(6, ['='])
+Nodes[0].add_to_edges(8, ['*'])
+Nodes[0].add_to_edges(10, ['/'])
+Nodes[0].add_to_edges(15, ['space'])
+
+Nodes[1].add_to_edges(1, ['letter', 'digit'])
+Nodes[1].add_to_edges(2, ['other'])
+
+Nodes[2].set_back_track()
+Nodes[2].set_final_state()
+
+Nodes[3].add_to_edges(4, ['other'])
+
+Nodes[4].set_back_track()
+Nodes[4].set_final_state()
+
+Nodes[5].set_final_state()
+
+Nodes[6].add_to_edges(7, ['='])
+Nodes[6].add_to_edges(9, ['other1'])
+
+Nodes[7].set_final_state()
+
+Nodes[8].add_to_edges(9, ['other2'])
+
+Nodes[9].set_back_track()
+Nodes[9].set_final_state()
+
+Nodes[10].add_to_edges(11, ['/'])
+Nodes[10].add_to_edges(13, ['*'])
+
+Nodes[11].add_to_edges(12, ['\n'])
+Nodes[11].add_to_edges(11, ['other3'])
+
+Nodes[12].set_final_state()
+
+Nodes[13].add_to_edges(13, ['other4'])
+Nodes[13].add_to_edges(14, ['*'])
+
+Nodes[14].add_to_edges(14, ['*'])
+Nodes[14].add_to_edges(13, ['other5'])
+
+Nodes[15].set_final_state()
+
+# ###################### end of assignments ##########################
 
 def is_valid(ch):           # Check if input character is valid
     valid_list = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<', '*', '=', '/']
@@ -85,68 +147,10 @@ def is_other5(ch):
     return False
 
 
-for i in range(0, 16):
-    Nodes.append(Node(i))
+def check_edges(ch):
+    state_edges =  Nodes[state].get_edges()
+    for i in state_edges:
 
-# #############  Add exit edges of each node ##################
-
-Nodes[0].add_to_edges(1, ["letter"])
-Nodes[0].add_to_edges(3, ["digit"])
-Nodes[0].add_to_edges(5, [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<'])
-Nodes[0].add_to_edges(6, ['='])
-Nodes[0].add_to_edges(8, ['*'])
-Nodes[0].add_to_edges(10, ['/'])
-Nodes[0].add_to_edges(15, ['space'])
-
-Nodes[1].add_to_edges(1, ['letter', 'digit'])
-Nodes[1].add_to_edges(2, ['other'])
-
-Nodes[2].set_back_track()
-Nodes[2].set_final_state()
-
-Nodes[3].add_to_edges(4, ['other'])
-
-Nodes[4].set_back_track()
-Nodes[4].set_final_state()
-
-Nodes[5].set_final_state()
-
-Nodes[6].add_to_edges(7, ['='])
-Nodes[6].add_to_edges(9, ['other1'])
-
-Nodes[7].set_final_state()
-
-Nodes[8].add_to_edges(9, ['other2'])
-
-Nodes[9].set_back_track()
-Nodes[9].set_final_state()
-
-Nodes[10].add_to_edges(11, ['/'])
-Nodes[10].add_to_edges(13, ['*'])
-
-Nodes[11].add_to_edges(12, ['\n'])
-Nodes[11].add_to_edges(11, ['other3'])
-
-Nodes[12].set_final_state()
-
-Nodes[13].add_to_edges(13, ['other4'])
-Nodes[13].add_to_edges(14, ['*'])
-
-Nodes[14].add_to_edges(14, ['*'])
-Nodes[14].add_to_edges(13, ['other5'])
-
-Nodes[15].set_final_state()
-
-# ###################### end of assignments ##########################
-
-file = open("input.txt", 'r')
-input_file = file.read()
-file.close()
-while_state = True
-pointer = 0
-
-
-# def check_edges():
 
 
 
@@ -154,13 +158,12 @@ def get_next_token():
     temp_lexeme = ""
     current_char = input_file[pointer]
     if is_valid(current_char):
-        #check_edges(current_char, pointer)
+        check_edges(current_char)
 
 
 
 while while_state:
-    print(input_file[pointer], end=" ")
+    get_next_token()
     if pointer == (len(input_file) - 1):
         while_state = False
     pointer += 1
-
