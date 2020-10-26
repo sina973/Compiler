e904ba30d57884917145f9316232c088c84cf026
sina973 <44430322+sina973@users.noreply.github.com>
2020-10-26 13:12:49 +0330

    Add files via upload

--- Scanner.py
+++ Scanner.py
@@ -3,6 +3,7 @@ class Node:
     def __init__(self, id):
         self.id = id
         self.exit_edge = {}
+        self.back_track = False
 
     def add_to_edges(self, node, expression):
         self.exit_edge[node] = expression
@@ -13,9 +14,15 @@ class Node:
     def get_name(self):
         return self.id
 
+    def set_back_track(self):
+        self.back_track = True
+
 
 Nodes = {}
+state = 'start'
 for i in range(0, 16):
     Nodes[i] = Node(i)
+
+Nodes[0].add_to_edges()
 
 
